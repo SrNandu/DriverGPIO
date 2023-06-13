f871c4393034ce59b1740b8ff12e4ec55bb5a066
SrNandu <octaula@gmail.com>
2023-06-13 17:18:26 -0300

    Python print values

--- Plot.py
+++ Plot.py
@@ -1,2 +1,13 @@
-f = open("/dev/GPIODriver", "r")
-value = f.read(1)
+import time
+
+f0 = open("/dev/GPIODriver0", "r")
+f1 = open("/dev/GPIODriver1", "r")
+
+while(True):
+    value0 = f0.read(1)
+    value1 = f1.read(1)
+
+    print(value0)
+    print(value1)
+
+    time.sleep(1)
